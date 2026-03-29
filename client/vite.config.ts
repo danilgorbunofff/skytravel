d41d4e769c229711d54dabb67a184b54578f7924
Danil <danil@Danils-MacBook-Air.local>
2026-03-29 19:03:50 +0200

    Change vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: ["skytravel-client-production-dc5f.up.railway.app"]
   }
 });
